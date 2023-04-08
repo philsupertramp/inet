10c6d4aa455ff4cfdfad0a0b84f72beafeacfbfe
Philipp Zettl <zettl.philipp@gmail.com>
2023-04-08 14:22:02 +0200

    fix thesis document

--- docs/tex/entrypoint.sh
+++ docs/tex/entrypoint.sh
@@ -1,23 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
 
-cd /usr/app
+BASE_PATH=$1
+BASE_FILENAME=$2
 
-PDF_BUILD='pdflatex -synctex=1 -interaction=nonstopmode -file-line-error -recorder -output-directory="build" "BA.tex"'
-BIB_BUILD='bibtex build/BA.aux'
+cd /usr/app/$BASE_PATH
+
+mkdir -p /usr/app/build
+PDF_BUILD='pdflatex -synctex=1 -interaction=nonstopmode --shell-escape --enable-write18 -file-line-error -recorder -output-directory="/usr/app/build" "/usr/app/${BASE_PATH}/${BASE_FILENAME}.tex"'
+BIB_BUILD='TEXMFOUTPUT=/usr/app/build bibtex /usr/app/build/${BASE_FILENAME}.aux'
+
+if [ ! -f "/usr/app/${BASE_PATH}/${BASE_FILENAME}.tex" ]
+then
+  echo "File not found."
+  exit
+else
+  echo "File found."
+fi
 
 echo "Running pdflatex (1/3)"
-eval "${PDF_BUILD}" > /dev/null
+eval "${PDF_BUILD}" > ../build/BA-1.log
 
 
 echo "Running bibtex (1/1)"
-eval "${BIB_BUILD}" 1> /dev/null
+eval "${BIB_BUILD}" > ../build/bibtex.log
 
 
 echo "Running pdflatex (2/3)"
-eval "${PDF_BUILD}" 1> /dev/null
+eval "${PDF_BUILD}" > ../build/BA-2.log
 
 
 echo "Running pdflatex (3/3)"
-eval "${PDF_BUILD}" 1> /dev/null
+eval "${PDF_BUILD}" > ../build/BA-3.log
+
+mkdir -p /usr/app/logs
+mv /usr/app/build/*.log /usr/app/logs
+mv /usr/app/build/${BASE_FILENAME}.pdf /usr/app/${BASE_PATH}/${BASE_FILENAME}.pdf
+/bin/rm -rf /usr/app/build/*
 
 exit 0
